061d420912bd4dc20615754be3c4b0ec776a0af6
shoshi4269 <sh6704269@gmail.com>
2022-03-28 15:01:50 +0300

    create product router page

---
+++ routes/product.js
@@ -0,0 +1,9 @@
+const router=require("express").Router();
+const productController=require("../controlers/product");
+
+router.get("",productController.selectAllProducts);
+router.get("/:id",productController.selectProductById);
+router.post("",productController.addProduct);
+router.delete("/``byId``/:id",productController.deleteProductById);
+
+module.exports=router;
