933cd423d446ae5bfa1cfd701f64e5bfb2ba4b86
shoshi4269 <sh6704269@gmail.com>
2022-03-28 15:20:28 +0300

    feat(appPage): use products router

---
+++ app.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const productRout = require("./routes/product");
+
+const app=express();
+
+mongoose.connect("mongodb://localhost:27017/chinese-sale")
+.then(console.log("mongodb connected"))
+.catch(err=>{console.log(err)});
+
+app.use(express.json())
+app.use("/product",productRout)
+app.listen(4500,()=>{console.log("waiting.....")})
